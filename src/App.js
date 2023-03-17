e5eeadc7695947c9d6df47e1b928c99dd3973a0c
James Trenholme <jamestrenholme@NIMBLE-00202.local>
2023-03-17 19:58:58 +0000

    Adds env variable and dummy data

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
-if (process.env.NODE_ENV === 'development') {
-  const { worker } = require('./mocks/browser')
-  worker.start()
+if (process.env.REACT_APP_ENV === "development") {
+  console.log(process.env.REACT_APP_ENV);
+  const { worker } = require("./mocks/browser");
+  worker.start();
 }
 
 function App() {
+  const [useData, setData] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await import("./mocks/data.json");
+      setData(results.rankings);
+    })();
+  }, []);
+
+  console.log(useData);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="sm">
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Mock Service Worker - Demo
+          </Typography>
+        </Box>
+      </Container>
     </div>
   );
 }
